6020519f3d039cd3399458c5be9048ed61d297b4
syed-imran <100760767+SyedsImran@users.noreply.github.com>
2024-02-20 17:52:36 +0530

    Create apisec_scanner_install_script.sh

---
+++ apisec_scanner_install_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+# Begin
+
+FX_HOST=$1
+FX_PORT=$2
+FX_SSL=$3
+FX_IAM=$4
+FX_KEY=$5
+FX_TAG=$6
+
+#install docker
+sudo curl -sSL https://get.docker.com/ | sh
+
+echo "Starting APIsec/Scanner" 
+echo "host=${FX_HOST}:${FX_PORT}"
+ 
+sudo docker run -d -e FX_HOST="${FX_HOST}" -e FX_PORT="${FX_PORT}" -e FX_SSL="${FX_SSL}" -e FX_IAM="${FX_IAM}" -e FX_KEY="${FX_KEY}" fxlabs/bot:${FX_TAG}
