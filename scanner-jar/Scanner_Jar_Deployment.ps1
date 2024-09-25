12db386a52bb516cda998a0bae7eafbfa7fa8a3b
SyedsImran <syedimran@apisec.ai>
2024-09-25 06:28:44 +0000

    Powershell script to deploy scanner with jar file

---
+++ scanner-jar/Scanner_Jar_Deployment.ps1
@@ -0,0 +1,26 @@
+# .\script.ps1 -fx_host "your-fx-host" -fx_key "your-fx-key" -fx_iam "your-fx-iam"
+param(
+    [string]$fx_host,
+    [string]$fx_key,
+    [string]$fx_iam
+)
+
+$botJarUrl = "https://raw.githubusercontent.com/apisec-inc/apisec-scripts/refs/heads/master/scanner-jar/bot.jar"
+$botJarPath = ".\bot.jar"
+$Env:SPRING_AMQP_DESERIALIZATION_TRUST_ALL = "true"
+$Env:FX_HOST = $fx_host
+$Env:FX_KEY = $fx_key
+$Env:FX_IAM = $fx_iam
+
+Write-Host "Downloading bot.jar..."
+Invoke-Expression ("curl -o " + $botJarPath + " " + $botJarUrl)
+
+if (Test-Path $botJarPath) {
+    Write-Host "bot.jar downloaded successfully."
+} else {
+    Write-Error "Failed to download bot.jar."
+    exit 1
+}
+
+Write-Host "Running bot.jar with parameters: fx-host=$fx_host, fx-key=$fx_key, fx-iam=$fx_iam"
+java -jar $botJarPath
